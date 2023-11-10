f4f527cdf86347f3908ee14aaa345a261e52b583
tao.ran <290535018@qq.com>
2023-11-10 12:17:32 +0800

    fix: typo of external dns version

--- modules/general/external-dns/main.tf
+++ modules/general/external-dns/main.tf
@@ -22,7 +22,7 @@ resource "helm_release" "external_dns" {
   name       = "external-dns"
   repository = "https://kubernetes-sigs.github.io/external-dns"
   chart      = "external-dns"
-  version    = "1.10.2"
+  version    = "1.10.1"
 
   set {
     name  = "serviceAccount.create"
